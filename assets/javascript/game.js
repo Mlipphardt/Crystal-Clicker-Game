9e8776c616fac1e6a5c77c0ed12b63af9fe050d8
Mlipphardt <matthewlipphardt@gmail.com>
2020-01-11 00:46:52 -0500

    Game now reacts to click on crystal, adds crystal value to score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,33 @@ $(document).ready(function() {
 
 
 let goalnumber = 0;
+let score = 0;
 
 $("#goalnumbertext");
 
+//Function to reset game
+function resetGame() {
+//Generates goal value, updates information for user
+    goalnumber = (Math.floor(Math.random() * 101) + 19);
+    console.log(goalnumber);
+    $("#goalnumbertext").text(goalnumber);
+
+//Generates randomized values for each crystal
+    $(".crystal").each(function(i, crystal){
+        let crystalValue = (Math.floor(Math.random() * 11) + 1);
+        $(crystal).attr("value", crystalValue);
+    });
+}
+
+resetGame();
+
+$(".crystal").on("click", function(){
+    score += parseInt($(this).attr("value"));
+    console.log(score); 
+}
+
+)
 
 
-goalnumber = (Math.floor(Math.random() * 101) + 19);
-console.log(goalnumber);
-$("#goalnumbertext").text(goalnumber);
-
-$('.testimonial').each(function(i, obj) {
-    //test
-});
-
-$("#goaltext").text(goalnumber);
-
-
-// goalnumber = (math.)
-$(".crystal").each(function(i, crystal){
-    let crystalValue = (Math.floor(Math.random() * 11) + 1);
-    $(crystal).attr("value", crystalValue);
-});
 
 })
